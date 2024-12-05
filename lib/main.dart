7e605c3ea14a569e1dee371fc68443c2a589e82d
Fatwah Firmansyah <fatwah091@gmail.com>
2024-12-05 19:50:38 +0700

    Bug on Theme

--- lib/main.dart
+++ lib/main.dart
@@ -26,6 +26,7 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    //TODO: Fixing Theme Bug
     return BlocBuilder<ThemeCubit, ThemeData>(
       builder: (context, state) {
         return MaterialApp(
